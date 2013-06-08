c4f740b09f40b9fac9ce690a67b5ea28f5b395d5
Nick Hodge <hodgenick@gmail.com>
2013-06-08 12:34:52 +1000

    refactor html work

--- HeadsTails WP8/MainPage.xaml.cs
+++ HeadsTails WP8/MainPage.xaml.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 using System.Windows.Navigation;
 using HeadsTails;
 using HeadsTailsPortableCode;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using Microsoft.Live;
 using Microsoft.Live.Controls;
 
 
